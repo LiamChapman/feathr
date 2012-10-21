57f95650072cc12f26cd8904b545d7a2a3c5d96a
Liam Chapman <patch5216@gmail.com>
2012-10-21 20:12:12 +0100

    removed some third party stuff and testing classes.

--- apps/pages.php
+++ apps/pages.php
@@ -1,13 +1,5 @@
 <?php
 
-$app->get('/log/check', function () use ($app) {
-	echo 'huzzah!';	
-})->user->auth();
-
-$app->get('/log/set', function () use ($app) {
-	echo 'session set';
-})->user->set();
-
 $app->get('/get/:string', function ($slug) use ($app) {	
 	if(isset($slug)) {
 		#$app->json_path = '/';
--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php version_compare(PHP_VERSION, '5.4') >= 0 ? require_once __DIR__ . '/5.4/feathr.php' : require_once __DIR__ . '/feathr.php'; ?>
 <?php
 
-$app  = new Feathr\FeathrApp('Feathr', array(
-	'user'
-));
+$app  = new Feathr\FeathrApp('Feathr');
 
 # less
 $app->get(':any.css', function ($file) {
 	$less = new Feathr\Extend\Less;
 	$less->parse($file);
 });
-
-# Sprockets - not working properly yet - speak to stu when ready.
-# Also uncomment rule in .htaccess.
-/*
-$app->get(':any.js', function ($file) {
-	$sprock = new Feathr\Extend\Sprockets;
-	$sprock->parse($file);
-});
-*/
 
 # home page
 $app->get('/', function () use ($app) {
